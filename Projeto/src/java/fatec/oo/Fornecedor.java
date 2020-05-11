be7203f2949fc9b8958bf5c22e146fcc48198b27
Débora Franco <44329473+Dehcbf@users.noreply.github.com>
2020-05-11 15:01:23 -0300

    Criação da classe Fornecedor
    Adição dos atributos.

---
+++ Projeto/src/java/fatec/oo/Fornecedor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.oo;
+
+/**
+ *
+ * @author DÉBORACRISTINABRANDÃ
+ */
+public class Fornecedor {
+    String nome;
+    String razao_social;
+    String cnpj;
+    String email;
+    String telefone;
+    String logradouro;
+}
