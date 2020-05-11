ee44b9bb60bd69ff65bfe9d12583a85625dd9662
Daniel <Daniel@X451C4>
2020-05-11 15:19:02 -0300

    getters and setters fornecedor

--- Projeto/src/java/fatec/oo/Fornecedor.java
+++ Projeto/src/java/fatec/oo/Fornecedor.java
@@ -10,10 +10,58 @@ package fatec.oo;
  * @author DÉBORACRISTINABRANDÃ
  */
 public class Fornecedor {
-    String nome;
-    String razao_social;
-    String cnpj;
-    String email;
-    String telefone;
-    String logradouro;
+    private String nome;
+    private String razao_social;
+    private String cnpj;
+    private String email;
+    private String telefone;
+    private String logradouro;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazao_social() {
+        return razao_social;
+    }
+
+    public void setRazao_social(String razao_social) {
+        this.razao_social = razao_social;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
 }
